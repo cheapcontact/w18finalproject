e5a5d14e4f73d0e77d454a87a1967af0695b4f35
cheapcontact <skessler@gmail.com>
2016-04-19 13:54:40 -0700

    added ability to rename columns and parse dates on data read

--- shared_utils.py
+++ shared_utils.py
@@ -1,10 +1,20 @@
 import pandas as pd
 import numpy as np
 
-def read_trip_data():
-    return pd.read_csv("201508_trip_data.csv")
+def read_trip_data(rename_columns=False, parse_dates=True):
+    date_columns = ["Start Date", "End Date"] if parse_dates else None
+    return read_data("201508_trip_data.csv", rename_columns,
+                     date_columns)
 
 
-def read_station_data():
-    return pd.read_csv("201508_station_data.csv")
+def read_station_data(rename_columns=False, parse_dates=True):
+    date_columns = ["installation"] if parse_dates else None
+    return read_data("201508_station_data.csv", rename_columns,
+                     date_columns)
 
+
+def read_data(file_name, rename_columns=False, parse_dates=None):
+    df = pd.read_csv(file_name, parse_dates=parse_dates)
+    if rename_columns:
+        df.rename(columns=lambda x: x.replace(" ", "_"), inplace=True)
+    return df
